681c9317484d6d44b4f27e27b9d79cdd204523a1
Christian Yao Komenan <kycdev@gmail.com>
2019-02-05 15:59:58 +0000

    commit

---
+++ user-server-deployment/dockerPush.bat
@@ -0,0 +1,2 @@
+docker tag mic-utilisateur-gestion-server 10.40.40.3:5000/mic-utilisateur-gestion-server
+docker push 10.40.40.3:5000/mic-utilisateur-gestion-server
---
+++ user-server-deployment/dockerRun.bat
@@ -0,0 +1,1 @@
+docker run -p 8080:8080 --add-host sib-database:10.40.40.9 mic-utilisateur-gestion-server:latest
